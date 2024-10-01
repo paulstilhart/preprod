ac69932bb3fa00a71b45918e6b8117e2ba5fde4f
paulstilhart <paul.stilhart@gmail.com>
2024-10-01 17:28:37 +0100

    avancee

--- js/script.js
+++ js/script.js
@@ -5,20 +5,22 @@ const body = document.querySelector("body");
 const header = document.querySelector("header");
 
 
-const swiper = new Swiper(".swiper_cards_homepage", {
+const swiper_thematiques = new Swiper(".swiper_thematiques", {
   slidesPerView: "auto", // S'adapte à la taille des slides définie par CSS
   spaceBetween: 16, // espace entre les slides
-  //evolution du padding en fonction de largeur d'écran  0 (16px) 768 (32px) 1024 (64px) 1280 (120px)
   slidesOffsetBefore:16,
   slidesOffsetAfter:16,//ne change pas
   grabCursor: false,
+  /*  
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper_pagination",
     clickable: true,
   },
+  */
+  //<!--<div class="swiper_pagination"></div>-->
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper_thematiques_next",
+    prevEl: ".swiper_thematiques_prev",
   },
   
   breakpoints: {
@@ -34,3 +36,35 @@ const swiper = new Swiper(".swiper_cards_homepage", {
     },
   },
 });
+
+function swiper_centre(containerClass, nextButtonClass, prevButtonClass = {}) {
+  return new Swiper(containerClass, {
+      effect: "coverflow",
+      slidesPerView: "auto",
+      grabCursor: true,
+      centeredSlides: true,
+      spaceBetween: 24,
+      loop: true,
+      coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+      },
+      navigation: {
+          nextEl: nextButtonClass,
+          prevEl: prevButtonClass,
+      },
+      breakpoints: {
+          768: {
+              spaceBetween: 32,
+          },
+      },
+  });
+};
+
+// Initialisation des Swipers
+const swiper_centre_1 = swiper_centre(".swiper_centre_1", ".swiper_centre_1_next", ".swiper_centre_1_prev");
+const swiper_centre_2 = swiper_centre(".swiper_centre_2", ".swiper_centre_2_next", ".swiper_centre_2_prev");
+const swiper_centre_3 = swiper_centre(".swiper_centre_3", ".swiper_centre_3_next", ".swiper_centre_3_prev");
